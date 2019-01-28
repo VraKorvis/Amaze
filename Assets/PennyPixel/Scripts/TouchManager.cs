fba83a31b74f988fff461548c8e81c53e6bfb969
VraKorvis <it.korvis@gmail.com>
2019-01-28 18:58:59 +0400

    prototype2.0 + lerp rotate camera when turn + lerp turn player

---
+++ Assets/PennyPixel/Scripts/TouchManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TouchManager : MonoBehaviour {
+    public static TouchManager instance;
+
+    public Rigidbody2D _characterRB;
+   
+
+    void Awake() {
+        instance = this;
+    }
+
+}
+
+
