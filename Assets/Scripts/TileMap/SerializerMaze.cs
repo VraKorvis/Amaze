2e783b77c1f1740e808b8a828dc0b9fc38b3cf5a
VraKorvis <it.korvis@gmail.com>
2019-02-13 21:27:57 +0400

    add testscene with tilemap palette with autotile

---
+++ Assets/Scripts/TileMap/SerializerMaze.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SerializerMaze : MonoBehaviour {
+
+    public void MazeToJson() {
+
+    }
+
+    public void JsonToMaze() {
+
+    }
+}
